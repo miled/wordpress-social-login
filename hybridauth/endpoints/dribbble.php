277f801d72bfb5b14091535a333b3e2163e4c947
miled <hybridauth@gmail.com>
2014-10-12 23:35:52 +0200

    add dribbble to the supported providers

---
+++ hybridauth/endpoints/dribbble.php
@@ -0,0 +1,11 @@
+<?php
+/*!
+* WordPress Social Login
+*
+* https://miled.github.io/wordpress-social-login | http://github.com/miled/wordpress-social-login
+*  (c) 2011-2014 Mohamed Mrassi and contributors | http://wordpress.org/extend/plugins/wordpress-social-login/
+*/
+
+define( 'WORDPRESS_SOCIAL_LOGIN_CUSTOM_ENDPOINT', 'Dribbble' );
+
+require_once( "../index.php" );
