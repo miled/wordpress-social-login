94d222ed9d5a37ea4ed4d7db4d3b948015dbda0a
miled <mohamed.b.miled@gmail.com>
2014-10-25 00:07:41 +0200

    update user guide to 2.2.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 $(function () { 
 	prettyPrint()
-	
-	// $('body').append( "<a href='https://github.com/hybridauth/WordPress-Social-Login'><img id='ribbon' alt='Fork me on GitHub' style='position: fixed; top: 37px; right: 0pt; z-index: 2; border: 0pt none; margin: 0pt; padding: 0pt;' src='https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png' /></a>" )
 
-	// $('#page').prepend( '<div class="alert-message info" style="margin-bottom:10px"><p style="line-height: 24px;"><b>Important:</b><br />This documentation has been updated to reflect the latest changes in the coming release of WordPress Social Login <b>2.2.1</b>. Things are partly different from the current version <b>2.1.6</b>.<br />To download WSL <b>2.2.1</b> and for more information, refer to <a href="release-preview-2.2.1.html" style="color:white;font-weight: bold; font-size: 15px;">&laquo; Preview the Upcoming Release and suggest any final changes &raquo;</b></a></p></div>' ) 
+	// why so sad :)
+	$('body').append( '<a href="https://github.com/miled/wordpress-social-login" target="_blank"><img alt="Fork me on GitHub" style="position: fixed; z-index: 2; border: 0pt none; margin: 0pt; padding: 0pt; right: 7px; top: 69px;" src="http://hybridauth.com/sf.net/wsl/new/assets/img/github.png" /></a><a href="https://wordpress.org/support/view/plugin-reviews/wordpress-social-login#postform" target="_blank"><img alt="Fork me on GitHub" style="position: fixed; z-index: 2; border: 0pt none; margin: 0pt; padding: 0pt; right: 15px; top: 125px;" src="http://hybridauth.com/sf.net/wsl/new/assets/img/rate.png" /></a>' )
 
+	// am lazy p
 	$('#content img').each(function() {
 		$(this).wrap("<a target='_blank' href='" + $(this).attr("src") + "'</a>")
 	})
+	
+	$('#content').prepend( '<div class="alert alert-warning"><h4 style="border: 0px none;">Information:</h4><p style="line-height: 24px;">This documentation has been updated to reflect the latest changes in the coming release <b>2.2.3</b>. Things are siltly different from the current version <b>2.2.2</b>.</p></div>' )
 })
