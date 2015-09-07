5b3de2c569d578392bd47aed0f2b74c5e3e60757
Nick Ivanter <nick@emcraft.com>
2015-09-07 20:43:38 +0300

    We don't need user's facebook friends.
    And don't want to scare him by asking the permission to see them.

--- hybridauth/Hybrid/Providers/Facebook.php
+++ hybridauth/Hybrid/Providers/Facebook.php
@@ -15,7 +15,7 @@
 class Hybrid_Providers_Facebook extends Hybrid_Provider_Model
 {
 	// default permissions, and a lot of them. You can change them from the configuration by setting the scope to what you want/need
-	public $scope = "email, public_profile, user_friends";
+	public $scope = "email, public_profile";
 
 	/**
 	* IDp wrappers initializer 
