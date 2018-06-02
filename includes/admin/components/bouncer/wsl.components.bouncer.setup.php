bf86aa47bd3ef3db61497db8373c0875cb3a5249
Miled <miled@users.noreply.github.com>
2018-06-02 07:34:16 +0100

    revert #224
    removes wsl_settings_bouncer_limit_to_one_profile_per_provider_per_user

--- includes/admin/components/bouncer/wsl.components.bouncer.setup.php
+++ includes/admin/components/bouncer/wsl.components.bouncer.setup.php
@@ -111,15 +111,6 @@ function wsl_component_bouncer_setup_accounts_linking()
 				</select>
 			</td>
 			</tr>
-			<tr>
-				<td width="200" align="right"><strong><?php _wsl_e("Limit to one social account per social network", 'wordpress-social-login') ?> :</strong></td>
-				<td>
-					<select name="wsl_settings_bouncer_limit_to_one_profile_per_provider_per_user">
-						<option <?php if( get_option( 'wsl_settings_bouncer_limit_to_one_profile_per_provider_per_user' ) == 1 ) echo "selected"; ?> value="1"><?php _wsl_e("Yes", 'wordpress-social-login') ?></option>
-						<option <?php if( get_option( 'wsl_settings_bouncer_limit_to_one_profile_per_provider_per_user' ) == 2 ) echo "selected"; ?> value="2"><?php _wsl_e("No", 'wordpress-social-login') ?></option>
-					</select>
-				</td>
-			</tr>
 		</table>  
 	</div>
 </div>
