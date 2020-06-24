3d2bfb565da2c833fa159cca5832e53140ec31dc
Aleksandar Nikolikj <aleksandar.nikolikj@ericsson.com>
2020-06-25 04:03:29 +1000

    Adding JWT Thirdparty lib

---
+++ hybridauth/library/src/Thirdparty/JWT/ExpiredException.php
@@ -0,0 +1,6 @@
+<?php
+namespace Hybridauth\Thirdpart\JWT;
+
+class ExpiredException extends \UnexpectedValueException
+{
+}
---
+++ hybridauth/library/src/Thirdparty/JWT/SignatureInvalidException.php
@@ -0,0 +1,6 @@
+<?php
+namespace Hybridauth\Thirdpart\JWT;
+
+class SignatureInvalidException extends \UnexpectedValueException
+{
+}
