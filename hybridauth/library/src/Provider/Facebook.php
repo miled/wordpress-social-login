b9c540552dc4f026b9fbe35ca39ab0df6645a2c7
sviluppomania <63558798+sviluppomania@users.noreply.github.com>
2020-09-23 19:17:45 +0200

    Correct Facebook API (v.8.0, current API)
    Correct Facebook API (v.8.0, current API)

--- hybridauth/library/src/Provider/Facebook.php
+++ hybridauth/library/src/Provider/Facebook.php
@@ -54,7 +54,7 @@ class Facebook extends OAuth2
     /**
      * {@inheritdoc}
      */
-    protected $apiBaseUrl = 'https://graph.facebook.com/v6.0/';
+    protected $apiBaseUrl = 'https://graph.facebook.com/v8.0/';
 
     /**
      * {@inheritdoc}
